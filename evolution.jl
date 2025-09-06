b518e480791f3463cd0936c3193c7ff3520617fd
Charles-21 <charles07@ciencias.unam.mx>
2025-09-06 17:16:17 -0600

    salvando datps dependientes solo de t

--- evolution.jl
+++ evolution.jl
@@ -21,12 +21,16 @@ function evolve() # Función principal de evolución
 
       # Guardando Pasos temporales
       #if n % strideT == 0
-        open("data/phi1.rt", "a") do io
+       open("data/phi1.t", "a") do io
+              write(io, "$(t)\t$(phi1[begin])\n")
+       end # del do
+
+       open("data/phi1.rt", "a") do io
               for j in 1:strideR:Nr
                   write(io, "$(t)\t$(r[j])\t$(phi1[j])\n")
               end # del for
             write(io, "\n")
-          end # del do
+       end # del do
       #end
 
 
